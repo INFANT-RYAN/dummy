2fd411a3232b9d75cf63e1b6cc04ca9221c7abf7
Infant-Ryan <ini0k17@gmail.com>
2024-09-12 12:31:01 +0530

    ioiji

---
+++ dummy/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () =>{
+    return(
+        <>
+            <div className="max-w-[100%]">
+                <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                    <div className="h-[250px] bg-black">
+                    </div>
+                    <div className="h-[250px] bg-green-500 lg:col-span-2">
+
+                    </div>
+                    
+                </div>
+
+            </div>
+        </>
+    )
+
+}
+
+export default Header
--- dummy/src/main.jsx
+++ dummy/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from  "react-dom/client"
+import "./index.css"
+import Header from "./components/Header/Header";
+const Applayout = ()=>{
+  return(
+    <>
+        <Header></Header>   
+    </>
+  )
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Applayout></Applayout>)
